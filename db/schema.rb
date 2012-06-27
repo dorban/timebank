2a82d25163f3b211c7f8ab88729a69d65bc37e1d
aury95 <auryannaowens@gmail.com>
2012-06-27 10:48:06 -0500

    added user it column to account table

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120627152604) do
+ActiveRecord::Schema.define(:version => 20120627154305) do
 
   create_table "accounts", :force => true do |t|
     t.decimal  "time"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "banks", :force => true do |t|
