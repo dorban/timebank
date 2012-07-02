2b8e5487da6f869261d2b0587044a8c5dad12e89
Dan Orban <orban.dan@gmail.com>
2012-07-01 23:43:30 -0500

    Fixed authentication issues by adding name field in Registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation
   acts_as_authentic do |c|
 #      c.validate_email_field = false
     end # block optional
