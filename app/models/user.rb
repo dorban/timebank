6a50543268ce5ab26435bc3b7f7bf3bac02163e8
Micah <mifisher@hopeschool.org>
2012-07-05 11:31:02 -0500

    added age on profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :name, :email, :password, :password_confirmation, :birthday
   acts_as_authentic do |c|
 #      c.validate_email_field = false
     end # block optional
