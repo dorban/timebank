78b286a41a7f5f3f00f298154b0824fb85033382
Lani Crowell <lcrowell@hopeschool.org>
2012-07-06 10:46:14 -0500

    edited users page & fixed some spelling errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
   before_filter :require_no_user, :only => [:new, :create]
-  before_filter :require_user, :only => [:show, :edit, :update]
+  before_filter :require_user, :only => [:show, :edit, :update, :index]
+  
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
   
   def new
     @user = User.new
